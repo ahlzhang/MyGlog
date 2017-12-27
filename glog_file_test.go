77a3c5f97dbc13d7336296ee7b79d6401ca0a438
zhangbob <4842622@qq.com>
2017-12-27 20:17:51 +0800

    init

---
+++ glog_file_test.go
@@ -0,0 +1,39 @@
+/** 
+ *
+ *
+ * User: zhangbob 
+ * Date: 2017/12/27 
+ * Time: 下午3:52 
+ */
+package glog
+
+import (
+	"testing"
+	"flag"
+	"os"
+)
+
+func TestMain(m *testing.M) {
+	flag.Set("log_dir", "/Users/zhangbob/go/src/myGlog/log/")
+	flag.Parse()
+
+	ret := m.Run()
+	os.Exit(ret)
+}
+
+func TestTTT(t *testing.T) {
+	//Info("===>")
+	//Flush()
+	//
+	//Warning("===>1")
+	//Flush()
+
+	Error("===>2")
+	Flush()
+
+	//Info("===>3")
+	//Flush()
+	//
+	//Info("===>4")
+	//Flush()
+}
